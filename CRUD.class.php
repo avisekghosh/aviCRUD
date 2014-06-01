d857d06b88f9ee7268bb2aeae3456a45607100c5
Avisek Ghosh <avisek93@gmail.com>
2014-06-01 08:23:03 +0530

    Update CRUD.class.php

--- CRUD.class.php
+++ CRUD.class.php
@@ -1,20 +1,16 @@
 <?php
-define( "DB_DSN", "your DSN" );
-define( "DB_USERNAME", "your username" );
-define( "DB_PASSWORD", "your password" );
 class CRUD{
 
 		public static function getRow($tableName, $idFieldName, $idValue, $data=false, $limit=false)
 		{
-		  $CRUD=new CRUD();
 			$conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
 			$return = false;
 			$sql="";
 			$sqlRetriveField="";
-			$sqlRetriveField=($data!=false) ? $CRUD->sqlFieldCreator($data) : "*";	
+			$sqlRetriveField=($data!=false) ? CRUD::sqlFieldCreator($data) : "*";	
 			$sql=($limit==false) ? "SELECT ".$sqlRetriveField." FROM ".$tableName." WHERE ".$idFieldName." = :id" : "SELECT ".$sqlRetriveField." FROM ".$tableName." WHERE ".$idFieldName." = :id ".$limit;
 			$st = $conn->prepare ( $sql );
-			$pdoParam=$CRUD->getPDOConstantType($idValue);
+			$pdoParam=CRUD::getPDOConstantType($idValue);
 			$st->bindValue( ":id", $idValue, $pdoParam );
 			$st->execute();
 			$return=$st->fetch();
@@ -22,12 +18,11 @@ class CRUD{
 			return $return;
 		} 
 		
-		public static function addRow($data, $tableName)
+		public static function addRow($tableName, $data)
 		{
-		  $CRUD=new CRUD();
 			$conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
 			$sql="";
-			$insertPartQuery=$CRUD->sqlFieldCreator($data,"insert");
+			$insertPartQuery=CRUD::sqlFieldCreator($data,"insert");
 			$sql="INSERT INTO ".$tableName." ".$insertPartQuery;
 			$st = $conn->prepare ( $sql );
 			$conn->beginTransaction();
@@ -35,12 +30,46 @@ class CRUD{
 			{
 				$escapeF=":".$fKey;
 				$escapeV=$field;
-				$pdoParam=$CRUD->getPDOConstantType($field);
+				$pdoParam=CRUD::getPDOConstantType($field);
 				$st->bindValue($escapeF , $escapeV , $pdoParam);
 			}
 			$st->execute();
 			$return = (($conn->lastInsertId())!=0) ? $conn->lastInsertId() : false; 
 			$conn->commit();
+			$conn=null;
+			return $return;
+		}
+		
+		public static function deleteRow($tableName, $idFieldName, $idValue)
+		{
+			$conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+			$sql="DELETE FROM ".$tableName." WHERE ".$idFieldName."=:id";
+			$st = $conn->prepare ( $sql );
+			$pdoParam=CRUD::getPDOConstantType($idValue);
+			$st->bindValue( ":id", $idValue, $pdoParam );
+			$st->execute();
+			
+			//checking wheather deleted or not
+			$sql="SELECT 1 FROM ".$tableName." WHERE ".$idFieldName."=:id";
+			$pdoParam=CRUD::getPDOConstantType($idValue);
+			$st->bindValue( ":id", $idValue, $pdoParam );
+			$st->execute();
+			$return=($row=$st->fetch()) ? false : true;
+			$conn=null;
+			return $return;
+		}
+		
+		public static function updateRow($tableName,$idFieldName, $idValue, $data)
+		{
+			$conn= new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+			$updateQueryPart=CRUD::sqlFieldCreator($data,"update");
+			$sql="UPDATE ".$tableName." ".$updateQueryPart." WHERE ".$idFieldName."=:id";
+			
+			$st = $conn->prepare ( $sql );
+			$pdoParam=CRUD::getPDOConstantType($idValue);
+			$st->bindValue( ":id", $idValue, $pdoParam );
+			$st->execute();
+			$return=(($st->rowCount())!=0) ? true : false;
 			$conn=null;
 			return $return;
 		}
@@ -54,11 +83,13 @@ class CRUD{
 			return $return;
 		}
 		
-		private function sqlFieldCreator($dataArr,$FLAG=FALSE)
+		public static function sqlFieldCreator($dataArr,$FLAG=FALSE)
 		{	
 			$sqlFieldString="";
 			$sqlValueString="";
 			$insertString="";
+			$updateString="";
+			
 			$count=(is_array($dataArr)) ? count($dataArr) : FALSE;
 			
 			if($FLAG==FALSE)
@@ -82,13 +113,27 @@ class CRUD{
 				$insertString.="(".$sqlFieldString.") VALUES (".$sqlValueString.")";
 			}
 			
+			if($FLAG=="update")
+			{
+				$counterFront=0;
+				foreach($dataArr as $fKey=>$field)
+				{	
+					$fieldV=(is_int($field)) ? $field : "'".$field."'";
+					$sqlFieldString.=(($count-1)==$counterFront)? $fKey." = ".$fieldV :  $fKey." = '".$field."', " ;
+					$counterFront++;
+				}
+				$updateString.="SET ".$sqlFieldString;
+			}
+			
 			if($FLAG==FALSE)
 			return $sqlFieldString;
 			if($FLAG=='insert')
 			return $insertString;
+			if($FLAG=='update')
+			return $updateString;
 		}
 		
-	  private function getPDOConstantType( $var )
+		public static function getPDOConstantType( $var )
 		{
 		  if( is_int( $var ) )
 			return PDO::PARAM_INT;
